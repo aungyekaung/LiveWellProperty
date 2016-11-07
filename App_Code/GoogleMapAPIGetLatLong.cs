162192d0d88a8d9be4e77b0fc8a5c3e7ca7265e0
CHEN YING <E0046735@iss.nus.edu.sg>
2016-11-07 11:31:01 +0800

    initial commit berlin

---
+++ App_Code/GoogleMapAPIGetLatLong.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for GoogleMapAPIGetLatLong
+/// </summary>
+public class GoogleMapAPIGetLatLong
+{
+    public static XElement GoogleMapAPIGeoCoding(string address)
+    {
+        // Use the Google Geocoding service to get information about the user-entered address
+        // See http://code.google.com/apis/maps/documentation/geocoding/index.html for more info...
+        var url = String.Format("http://maps.google.com/maps/api/geocode/xml?address={0}&sensor=false", HttpContext.Current.Server.UrlEncode(address));
+
+        // Load the XML into an XElement object (whee, LINQ to XML!)
+        var results = XElement.Load(url);
+
+        // Check the status
+        var status = results.Element("status").Value;
+        if (status != "OK" && status != "ZERO_RESULTS")
+            // Whoops, something else was wrong with the request...
+            throw new ApplicationException("There was an error with Google's Geocoding Service: " + status);
+
+        return results;
+    }
+}
---
+++ Scripts/GoogleMapHelpers.js
@@ -0,0 +1,27 @@
+﻿function init_map(map_canvas_id, lat, lng, zoom, markers) {
+    var myLatLng = new google.maps.LatLng(lat, lng);
+    
+    var options = {
+        zoom: zoom,
+        center: myLatLng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    
+    var map_canvas = document.getElementById(map_canvas_id);
+
+    var map = new google.maps.Map(map_canvas, options);
+
+    if (markers && markers.length > 0) {
+        var bounds = new google.maps.LatLngBounds();
+
+        for (var i = 0; i < markers.length; i++) {
+            var marker = new google.maps.Marker(markers[i]);
+            marker.setMap(map);
+
+            bounds.extend(marker.getPosition());
+        }
+
+        map.fitBounds(bounds);
+        map.setCenter(bounds.getCenter());
+    }
+}
