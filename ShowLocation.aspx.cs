f2ed24068775d1631f29c7aaf539c402b5646954
Vijaya <Vijaya>
2016-11-07 12:47:12 +0800

    initial commit sowjanya

---
+++ ShowLocation.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class ShowLocation : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!Page.IsPostBack)
+        {
+            var address = Request.QueryString["Address"];
+            if (string.IsNullOrEmpty(address))
+                Response.Redirect("FindProperty.aspx");
+
+
+            // Get the lat/long info about the address
+            var results = GoogleMapAPIGetLatLong.GoogleMapAPIGeoCoding(address);
+
+            lblAddress.Text = address;
+
+            // Set the latitude and longtitude parameters based on the address being searched on
+            dsSearchResults.SelectParameters["Latitude"].DefaultValue = results.Element("result").Element("geometry").Element("location").Element("lat").Value;
+            dsSearchResults.SelectParameters["Longitude"].DefaultValue = results.Element("result").Element("geometry").Element("location").Element("lng").Value;
+
+            lvSearchResults.DataBind();
+        }
+    }
+}
---
+++ news.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="news.aspx.cs" Inherits="news" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <asp:HyperLink ID="linkHome" runat="server"
+                        navigateurl="~/Default.aspx">Home</asp:HyperLink>
+    </div>
+    </form>
+</body>
+</html>
